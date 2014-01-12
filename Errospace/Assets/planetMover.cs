9bd6d54adcd3615c5cfc94ad3e1d63cdd342bb61
crentagon <kiefer.yap@gmail.com>
2014-01-12 19:00:08 +0800

    Planet is now movable.

---
+++ Errospace/Assets/planetMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planetMover : MonoBehaviour {
+	
+	public Vector3 jumpVelocity;
+	public Transform BlackBall;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector2 position = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+		//BlackBall.localPosition = position;
+		if (Input.GetMouseButtonDown (0)) {
+			float mousex, mousey;
+			Vector2 mousepos;
+			mousex = Input.mousePosition.x;
+			mousey = Input.mousePosition.y;
+			mousepos = Camera.main.ScreenToWorldPoint(new Vector2 (mousex,mousey));
+			print(mousepos);
+			BlackBall.localPosition = mousepos;
+		}
+
+		//if (Input.GetButtonDown ("Jump"))
+			//rigidbody2D.AddForce(jumpVelocity);
+	}
+}
