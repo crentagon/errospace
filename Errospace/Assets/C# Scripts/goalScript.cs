3b88042f5a4f813beb42cc996c39f2f626e40384
crentagon <kiefer.yap@gmail.com>
2014-01-16 22:21:04 +0800

    Latest changes.

--- Errospace/Assets/C# Scripts/goalScript.cs
+++ Errospace/Assets/C# Scripts/goalScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class goalScript : MonoBehaviour {
 		
 	Collider2D firstCollider;
+	public bool showNextLevel = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,8 +17,6 @@ public class goalScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update(){
-		var buttonWidth = 150;
-		var buttonHeight = 30;
 
 		if(firstCollider != null && firstCollider.transform.name == "Rocket"){
 			//Rocket must stop at the goal position
@@ -25,12 +24,18 @@ public class goalScript : MonoBehaviour {
 			rocketScript.isActive = false;
 			rocketScript.rigidbody2D.velocity = new Vector2 (0, 0);
 			rocketScript.transform.position = transform.position - new Vector3 (0,0,1);
+			showNextLevel = true;
+//			print("Game finished!");
+		}
+	}
 
-			//Display next level button
-			if (GUI.Button (new Rect ((Screen.width-buttonWidth)/2,(Screen.height-buttonHeight+50)/2,buttonWidth, buttonHeight), "Next Level")) {
-				Application.LoadLevel(Application.loadedLevel+1);
-			}
-			print("Game finished!");
+	void OnGUI () {
+		var buttonWidth = 150;
+		var buttonHeight = 30;
+
+		//Display next level button
+		if (showNextLevel && GUI.Button (new Rect ((Screen.width - buttonWidth) / 2, (Screen.height - buttonHeight + 50) / 2, buttonWidth, buttonHeight), "Next Level")) {
+			Application.LoadLevel (Application.loadedLevel + 1);
 		}
 	}
 }
