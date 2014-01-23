0e48f5bacbeb58b033893c717570f650e246132c
tastelikecoke <jmerc87@gmail.com>
2014-01-23 20:12:06 +0800

    added trail effects, issue: goal not working, kiefer pls

--- Errospace/Assets/C# Scripts/rocketMover.cs
+++ Errospace/Assets/C# Scripts/rocketMover.cs
@@ -9,6 +9,7 @@ public class rocketMover : MonoBehaviour {
 	public bool isActive;
 
 	Collider2D firstCollider;
+	public Transform planet;
 
 	void OnTriggerEnter2D(Collider2D collider){
 		firstCollider = collider;
@@ -18,14 +19,20 @@ public class rocketMover : MonoBehaviour {
 		rigidbody2D.velocity = new Vector2 (0, 2);
 	}*/
 	void Update(){
-		if(firstCollider != null && firstCollider.transform.name != "LevelBoundaries" && isActive){
-			Vector3 offset = transform.position - firstCollider.transform.position;
+		if(isActive){			
+			print("making trails.");
+			trailMaker.Instance.makeTrail(transform.position);
+			// firstCollider != null && firstCollider.transform.name != "LevelBoundaries" 
+			Vector3 offset = transform.position - planet.transform.position;
 			float mag = offset.magnitude;
 			offset.Normalize ();
 			print(offset);
 			Vector2 force = new Vector2 (offset.x / mag / mag, offset.y / mag / mag);
 
 			rigidbody2D.velocity = rigidbody2D.velocity - force;
+		}
+		else{
+			rigidbody2D.velocity = new Vector2 (0, 0);
 		}
 	}
 
