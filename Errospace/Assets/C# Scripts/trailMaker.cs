76a96f67cfeb08dc12062c7f8a5e59f1bd863d51
tastelikecoke <jmerc87@gmail.com>
2014-01-23 22:07:50 +0800

    added changes; forgot to add previously

---
+++ Errospace/Assets/C# Scripts/trailMaker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trailMaker : MonoBehaviour {
+
+	// Use this for initialization
+	public static trailMaker Instance;
+	public ParticleSystem trailEffect;
+	void Awake(){
+		if(Instance != null){
+			print("shizz");
+			return;
+		}
+		Instance = this;
+	}
+	public void makeTrail(Vector3 position){
+		instantiate(trailEffect, position);
+	}
+
+	private ParticleSystem instantiate(ParticleSystem prefab, Vector3 position){
+		ParticleSystem newParticleSystem = Instantiate(
+			prefab,
+			position,
+			Quaternion.identity
+			) as ParticleSystem;
+
+		Destroy(
+		newParticleSystem.gameObject,
+		newParticleSystem.startLifetime
+		);
+		return newParticleSystem;
+	}
+	// Update is called once per frame
+
+}
